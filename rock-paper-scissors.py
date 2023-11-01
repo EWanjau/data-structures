9c20e88abb8b4c41abcfbd533e1bb6f9b1d82270
EWanjau <wamwanjau@gmail.com>
2023-11-01 07:45:41 +0300

    Updated Input Validation

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -28,29 +28,41 @@ scissors = '''
 ---.__(___)
 '''
 # Write your code below this line 👇
-feedback = input("Enter 1 for Rock, 2 for Paper, or 3 for Scissors")
+feedback = input("Enter 1 for Rock, 2 for Paper, or 3 for Scissors:")
 comppick = random.randint(1, 3)
-if feedback == 1:
-    print("You chose: {}".rock)
+
+if feedback == '1':
+    print("You chose: {}".format(rock))
     if comppick == 2:
-        print("Computer Chose:{}".paper)
+        print("Computer Chose:{}".format(paper))
         print('You Lose')
     elif comppick == 3:
-        print("Computer Chose:{}".scissors)
+        print("Computer Chose:{}".format(scissors))
         print('You Win')
-elif feedback == 2:
-    print("You chose: {}".paper)
+    else:
+        print("Computer Chose:{}".format(rock))
+        print("Repeat")
+elif feedback == '2':
+    print("You chose: {}".format(paper))
     if comppick == 1:
-        print("Computer Chose:{}".rock)
+        print("Computer Chose:{}".format(rock))
         print('You Win')
     elif comppick == 3:
-        print("Computer Chose:{}".scissors)
+        print("Computer Chose:{}".format(scissors))
         print('You Lose')
-else:
-    print("You chose: {}".scissors)
+    else:
+        print("Computer Chose:{}".format(paper))
+        print("Repeat")
+elif feedback == '3':
+    print("You chose: {}".format(scissors))
     if comppick == 1:
-        print("Computer Chose:{}".rock)
+        print("Computer Chose:{}".format(rock))
         print('You Lose')
     elif comppick == 2:
-        print("Computer Chose:{}".paper)
+        print("Computer Chose:{}".format(paper))
         print('You Win')
+    else:
+        print("Computer Chose:{}".format(scissors))
+        print("Repeat")
+else:
+    print("Sorry, Enter A Valid Option")
