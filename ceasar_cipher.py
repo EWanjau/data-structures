00047c80ba2844cbb8dc824ded31597d9adbdc2c
EWanjau <wamwanjau@gmail.com>
2023-11-08 23:28:23 +0300

    added an encrypting function to the module

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -1,12 +1,34 @@
 #!/usr/bin/python3
+"""This Module has an encrypting function for messages
+"""
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def greet(name, location):
-    print('Hello {}:'.format(name))
-    print('There is my name')
-    print('Do I hear you Over in {}'.format(location))
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+# TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
 
-# positional arguments, assign the data to variable automatically how they are ordered
-greet("Rungiri", "Wamuti")
-# Keyword arguments, specify what data goes to what variable
-greet(name="Erasto", location="Rungiri")
+def encrypt(words_to_encrypt=text, no_of_shifts=shift):
+    for i in words_to_encrypt:
+        pos_in_alphabet = alphabet.index(words_to_encrypt[i])
+        new_position = pos_in_alphabet + no_of_shifts
+        words_to_encrypt[i] = alphabet[new_position]
+    print("The Encoded Text is: ".format(words_to_encrypt))
+
+# TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+# e.g.
+# plain_text = "hello"
+# shift = 5
+# cipher_text = "mjqqt"
+# print output: "The encoded text is mjqqt"
+
+# HINT: How do you get the index of an item in a list:
+# https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+# 🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+
+# TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
